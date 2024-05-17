91624ae64696358330cc4ca6ea3f0dbeb77a6d93
Tsai6666 <tsai20040116@gmail.com>
2024-05-17 15:00:18 +0800

    2024-05-17

---
+++ 0517/ListViewDemo/app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.listviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.telecom.TelecomManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView lv;
+    private  String[] cities;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cities = getResources().getStringArray(R.array.cities);
+        lv = (ListView) findViewById(R.id.listview);
+
+
+
+       lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView output = (TextView) findViewById(R.id.lblOutput);
+                output.setText("你是住在: " + cities[position]);
+                Toast.makeText(MainActivity.this,"你是住在: " + cities[position],
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        TextView output = (TextView) findViewById(R.id.lblOutput);
+        output.setText("你是住在: " + cities[position]);
+        Toast.makeText(MainActivity.this,"你是住在: " + cities[position],
+                Toast.LENGTH_SHORT).show();
+    }
+}
